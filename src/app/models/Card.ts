d211bd434c1c7bf37992c0eb50139b6d8b74f765
Yonni GUERRAULT <yonni4@gmail.com>
2025-07-12 23:00:44 +0200

    ajout sens secondaires

--- src/app/models/Card.ts
+++ src/app/models/Card.ts
@@ -1,7 +1,9 @@
 export type Card = {
   proposal: string;
+  more?: string[]; // Ajout du champ "more" pour les significations supplémentaires
   choices: {
     label: string;
+    more?: string[]; // Ajout du champ "more" pour les significations supplémentaires
     correct: boolean;
   }[];
 }
